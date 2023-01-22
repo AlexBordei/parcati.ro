6a89745d6b718b862255407e855f0d2fa8bfc92a
Alex Bordei <alex.bordei@whiz.ro>
2023-01-22 10:31:43 +0200

    Adding font assets Adding SplashScreen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: Align(
+              alignment: Alignment.topRight,
+              child: FractionallySizedBox(
+                heightFactor: 0.7,
+                child: Image.asset(
+                  'assets/images/splash_side.png',
+                ),
+              ),
+            ),
+          ),
+          const Expanded(
+            flex: 2,
+            child: Text(
+              'Parcați.ro',
+              style: TextStyle(
+                color: Color(0xFF192342),
+                fontSize: 48,
+                fontFamily: 'Lato',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Image.asset(
+                'assets/images/splash_bottom.png',
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
